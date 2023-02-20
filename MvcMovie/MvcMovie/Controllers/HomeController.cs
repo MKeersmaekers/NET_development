d8eddb83f7c51b3cad2224c2ea53f0e311b54ab1
milanKeersmaekers <r0889348@student.thomasmore.be>
2023-02-20 21:31:51 +0100

    tweede les

---
+++ MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+using System.Diagnostics;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id, string name, int counter)
+        {
+            ViewData["name"] = name;
+            ViewData["counter"] = counter;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
