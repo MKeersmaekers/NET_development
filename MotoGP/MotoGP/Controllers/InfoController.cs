01ac1b59b2ceb667cd6b27e27a181ca5705ce676
Milan Keersmaekers <91118306+MKeersmaekers@users.noreply.github.com>
2023-02-21 13:52:36 +0100

    motogp part 1 alles af behalve laatste part nog

---
+++ MotoGP/MotoGP/Controllers/InfoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MotoGP.Models;
+
+namespace MotoGP.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListRaces()
+        {
+            ViewData["BannerNr"] = 0;
+            return View();
+        }
+
+        public IActionResult BuildMap()
+        {
+            List<Race> races = new List<Race>();
+
+            races.Add(new Race() { RaceID = 1, X = 517, Y = 19, Name = "Assen" });
+            races.Add(new Race() { RaceID = 2, X = 859, Y = 249, Name = "Losail Circuit" });
+            races.Add(new Race() { RaceID = 3, X = 194, Y = 428, Name = "Autódromo Termas de Río Hondo" });
+
+            ViewData["BannerNr"] = 0;
+
+            return View();
+        }
+    }
+}
