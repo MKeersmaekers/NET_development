ea673cd381028ce5c798690b85dea8a9584db35a
milanKeersmaekers <r0889348@student.thomasmore.be>
2023-02-28 09:07:08 +0100

    les 3

---
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MovieContext _context;
+
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult List()
+        {
+            List<Movie> movies = _context.Movies.ToList();
+
+            return View(movies);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var movie = _context.Movies
+                            .Include(m => m.Rating)
+                            .SingleOrDefault(m => m.MovieID == id);
+
+            return View(movie);
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["Ratings"] =
+                new SelectList(_context.Ratings.OrderBy(r => r.Name),
+                               "RatingID",
+                               "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(movie);
+        }
+
+    }
+}
--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,9 +1,17 @@
-﻿namespace MvcMovie.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
 {
     public class Movie
     {
         public int MovieID { get; set; }
+        [Display(Name = "Film Title")]
         public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true,
+          DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Release date")]
         public DateTime? ReleaseDate { get; set; }
         public string Genre { get; set; }
         public int? Price { get; set; }
