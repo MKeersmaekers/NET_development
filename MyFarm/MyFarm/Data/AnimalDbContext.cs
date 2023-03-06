745adcd57c187694e85595943bcbdcaa0b45a3b0
milanKeersmaekers <r0889348@student.thomasmore.be>
2023-03-06 15:03:14 +0100

    les 4

---
+++ MyFarm/MyFarm/Data/AnimalDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFarm.Models;
+
+namespace MyFarm.Data
+{
+    public class AnimalDbContext : DbContext
+    {
+        public AnimalDbContext(DbContextOptions<AnimalDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Animal>().ToTable(nameof(Animal));
+        }
+    }
+
+}
---
+++ MyFarm/MyFarm/Models/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFarm.Models
+{
+    public class Animal
+    {
+        public int AnimalID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Sex { get; set; }
+    }
+}
