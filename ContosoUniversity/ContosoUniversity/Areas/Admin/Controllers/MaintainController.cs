d6a36a497e12af0dfdd76e35b1cc8ae69f356598
milanKeersmaekers <r0889348@student.thomasmore.be>
2023-03-06 15:42:01 +0100

    les 4 einde

---
+++ ContosoUniversity/ContosoUniversity/Areas/Admin/Controllers/MaintainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoUniversity.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MaintainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
