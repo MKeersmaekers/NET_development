54faa861f7e65720243f4793c9904ed605b6e4b0
milanKeersmaekers <r0889348@student.thomasmore.be>
2023-03-13 15:42:00 +0100

    les 5

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -1,10 +1,16 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
 {
 	public class Student
 	{
 		public int ID { get; set; }
 		public string LastName { get; set; }
 		public string FirstMidName { get; set; }
+		[NotMapped]
+		public string FullName { get {
+				return FirstMidName + " " + LastName;
+					} }
 		public DateTime EnrollmentDate { get; set; }
 
 		public ICollection<Enrollment>? Enrollments { get; set; }
---
+++ MusicStore/MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicStore.Data;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public StoreController(StoreContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ListGenres()
+        {
+            var genres = _context.Genres.OrderBy(m => m.Name).ToList();
+
+            return View(genres);
+        }
+    }
+}
